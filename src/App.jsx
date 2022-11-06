5d8118ab88f383208bbcf179259f275ab9f0227f
Pierre Bourgeois <pbourgeois@gumqc.onmicrosoft.com>
2022-11-06 09:04:13 -0500

    Basics : variables and mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App(){
-    return <h1>Myapp</h1>
+
+    const title = 'blog post'
+    const body = 'My content'
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'}
+    ]
+
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            <h2>Comments</h2>
+            <p>{`Number of comments: ${comments.length}`}</p>
+            <ul>
+                {comments && comments.map(
+                    (comment, index) => <li key={index}>{comment.text}</li>
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default App
