9ba2a1dfffbde3aafa1a0960d5b03ff8399a048b
Pierre Bourgeois <pbourgeois@gumqc.onmicrosoft.com>
2022-11-06 09:35:29 -0500

    conditionnals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
 function App(){
-    return <h1>Myapp</h1>
+
+    const title = 'blog post'
+    const body = 'My content'
+    const commentsData = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'}
+    ]
+
+    const loading = false;
+
+    const comments = (
+        <>
+        {commentsData && (
+        <>
+            <h2>Comments</h2>
+            <p>{`Number of comments: ${commentsData.length}`}</p>
+            
+                <ul>
+                    {commentsData.map(
+                        (comment, index) => <li key={index}>{comment.text}</li>
+                    )}
+                </ul> 
+            </>
+            ) }
+        </>
+    )
+
+    if(loading) return <h1>Loading ...</h1>
+
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            { commentsData && comments}
+        </div>
+    )
 }
 
 export default App
